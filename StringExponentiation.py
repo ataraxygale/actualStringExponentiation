02ea73a2476e979beccdf934ea85372a89fc4f94
Angel <94824625+ataraxygale@users.noreply.github.com>
2025-03-26 09:07:50 -0600

    Add files via upload

---
+++ StringExponentiation.py
@@ -0,0 +1,23 @@
+
+
+##String Exponentiation for Python
+
+string = input("Enter the string you would like exponentiated: \n")
+expAmount = int(input("Enter the number of times you would like this exponentiated: \n"))
+
+
+exponentTwo = string*2
+expResult = exponentTwo*expAmount
+print("This is your exponentiated string: ", expResult)
+
+
+
+
+
+
+
+
+
+
+
+
